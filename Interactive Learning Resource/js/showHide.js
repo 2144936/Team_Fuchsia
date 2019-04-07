7b1064406ef4852e367c9255a683f80973a92566
ArnelArnel <arnel.brightside@gmail.com>
2019-04-07 21:54:22 +0800

    Added redirection link to activity from json

--- Interactive Learning Resource/js/showHide.js
+++
@@ -1,9 +0,0 @@
-var id = "";
-function show() {
-  var x = document.getElementById("showMe");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
